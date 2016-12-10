8e8055182b3ff741dd86c12cdffa98ffa87917bb
Nikhil Kothari <nikhilko@google.com>
2016-12-10 13:31:23 -0800

    Initial source and build setup

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# Create the build directory with inputs (sources) and outputs (binaries).
+# Ensure it is empty, to build from clean state.
+mkdir -p build
+rm -rf build
+mkdir -p build
+
+# Copy source files
+cp setup.py build
+cp -r src build/tensorfx
+
+# Generate the README expected by PyPI from original markdown
+curl --silent http://c.docverter.com/convert \
+  -F from=markdown \
+  -F to=rst \
+  -F input_files[]=@README.md > build/README.rst
+
+# Finally, build
+pushd build > /dev/null
+python setup.py sdist > setup.log
+popd > /dev/null
+
+echo 'Build completed successfully!'
+
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# Copyright 2016 TensorLab. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+
+# To publish to PyPi, use:
+# python setup.py bdist_wheel upload -r pypi
+
+import datetime
+import setuptools
+
+version = '0.1.' + datetime.datetime.now().strftime("%y%m%d%H%M")
+
+setuptools.setup(
+  name='tensorfx',
+  version=version,
+  namespace_packages=['tensorfx'],
+  packages=[
+    'tensorfx.models'
+  ],
+  install_requires=[
+  ],
+  author='Nikhil Kothari',
+  author_email='nikhilk@twitter',
+  url='https://github.com/TensorLab/tensorfx',
+  license="Apache Software License",
+  description='TensorFX Framework',
+  keywords=[
+    'TensorLab',
+    'TensorFlow',
+    'Machine Learning',
+    'Deep Learning',
+    'Google'
+  ],
+  classifiers=[
+  ]
+)
+
